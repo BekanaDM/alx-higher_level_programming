de22e21dc9543c70aec05c3b6f1dc41122144abd
BekanaDM <bekdmeta@gmail.com.com>
2023-05-30 04:17:12 -0700

    m

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+" " " write class that defines square by size 
+" " "
 
 class Square:
+    " " " methods which define size
+    " " "
     def __init__(self, size):
         self.__size = size
 
